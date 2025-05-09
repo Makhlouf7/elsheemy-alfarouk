4da9db71611ce7fcdc9382e84a9002dc392bee3b
Aly Makhlouf <ahmedmak012.2005@gmail.com>
2025-05-09 04:19:03 +0300

    finished creating/viewing suppliers

--- src/main/main.js
+++ src/main/main.js
@@ -1,14 +1,26 @@
-const { BrowserWindow, app } = require("electron");
+const { BrowserWindow, app, ipcMain } = require("electron");
 const path = require("path");
+const mongoose = require("mongoose");
+const Supplier = require("../models/suppliers");
+const dotenv = require("dotenv");
+dotenv.config({ path: "config.env" });
+
+const DB = process.env.DATABASE_URL.replace(
+  "<db_password>",
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(DB)
+  .then(() => console.log("Connected to database"))
+  .catch((err) => console.log("Couldn't connect to database"));
 
 const createWindow = () => {
   const mainWindow = new BrowserWindow({
-    width: 830,
-    height: 630,
+    fullscreen: true,
+    icon: path.join(__dirname, "../assets/logo_favicon.ico"),
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
     },
-    icon: path.join(__dirname, "../assets/logo_favicon.ico"),
   });
 
   mainWindow.loadFile("../renderer/pages/incoming/index.html");
@@ -21,4 +33,41 @@ app.whenReady().then(() => {
 // Adding an event for macOs as even if all windows are closed it doesn't close the process so we are telling it if its not windows OS and all windows have been closed kill the process
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") app.quit();
+});
+
+// Channels Communications
+const models = {
+  Supplier,
+  // Add more models as needed
+};
+
+ipcMain.handle("get:all", async (event, modelName) => {
+  const Model = models[modelName];
+  try {
+    const data = await Model.find();
+    return data;
+  } catch (err) {
+    console.error(`DB error on ${modelName}:`, err);
+  }
+});
+
+ipcMain.handle("get:byId", async (event, { modelName, id }) => {
+  try {
+    const Model = models[modelName];
+    const doc = await Model.findById(id);
+    return { success: true, data: doc };
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+ipcMain.handle("create:doc", async (event, { modelName, data }) => {
+  const Model = models[modelName];
+  console.log(data);
+  try {
+    await Model.create(data);
+    return { success: true };
+  } catch (err) {
+    console.error(`DB error creating ${modelName}:`, err);
+  }
 });
